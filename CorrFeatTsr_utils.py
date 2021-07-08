f33413db842ddbb7cf35f96753118aaaa1a67fae
Animadversio <binxu.wang@wustl.edu>
2021-07-07 21:53:09 -0500

    Add Utils

--- CorrFeatTsr_utils.py
+++ CorrFeatTsr_utils.py
@@ -9,6 +9,16 @@ mpl.rcParams['pdf.fonttype'] = 42
 mpl.rcParams['axes.spines.right'] = False
 mpl.rcParams['axes.spines.top'] = False
 
+def saveallforms(figdirs, fignm, figh=None, fmts=["png","pdf"]):
+    if type(figdirs) is str:
+        figdirs = [figdirs]
+    if figh is None:
+        figh = plt.gcf()
+    for figdir in figdirs:
+        for sfx in fmts:
+            figh.savefig(join(figdir, fignm+"."+sfx), bbox_inches='tight')
+
+
 def area_mapping(num):
     if num <= 32: return "IT"
     elif num <= 48 and num >= 33: return "V1"
